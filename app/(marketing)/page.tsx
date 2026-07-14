4c1bc117dd696b031ffd177c951f60d80150933c
Chigozie Ohakwe <chigozieohakwej@gmail.com>
2026-07-14 08:01:02 +0100

    learn more about buttons

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,52 +1,14 @@
 // src/app/(marketing)/page.tsx
 
-import Dreambox from "../components/business/Dreambox";
-import Nysc from "../components/business/Nysc";
-import Rove from "../components/business/Rove";
-import EstateApp from "../components/marketing/EstateApp";
 
-// import { Hero } from "../components/marketing/Hero";
-import { PartnerStrip } from "../components/marketing/PartnerStrip";
-// import { Hero } from "../components/marketing/Hero";
-// import { PartnerStrip } from "../components/marketing/PartnerStrip";
-import { ServicesGrid } from "../components/marketing/ServicesGrid";
-import UpdateStrip from "../components/marketing/UpdateStrip";
-// import { BusinessSpotlight } from "@/components/marketing/BusinessSpotlight";
-// import { ServicesGrid } from "@/components/marketing/ServicesGrid";
-// import { TalentAcademy } from "@/app/components/marketing/TalentAcademy";
 
 export default function LandingPage() {
   return (
     <main className="min-h-screen ">
-      {/* 1. The "Command Center" Hero Section */}
-      {/* <Hero /> */}
-      {/* <UpdateStrip /> */}
-      {/* 2. The "Buttons" Ecosystem (Services) */}
-      {/* <Hero/> */}
-      <ServicesGrid />
-
-      {/* <PartnerStrip/> */}
+      
       
 
-      {/* ROVE */}
-      {/* <Rove /> */}
-      {/* <Nysc /> */}
-      {/* <Dreambox /> */}
-
-      {/* Who are we? */}
-
-      {/* 3. The Business Partners (Visibility Section) */}
-      {/* <BusinessSpotlight /> */}
-      {/* <PartnerStrip /> */}
-
-      {/* 4. The Talent & Academy (Builders & Students) */}
-      {/* <TalentAcademy /> */}
-
-      {/*  */}
-
-
-      {/* NEW APP */}
-      {/* <EstateApp /> */}
+      
 
     </main>
   );
